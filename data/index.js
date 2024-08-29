2187cb4140c82a43918b81e8e51cc079741d3b1a
Q Chen <qche.au@gmail.com>
2024-08-29 17:31:53 +1000

    add data for url navs and db

---
+++ data/index.js
@@ -0,0 +1,16 @@
+const url = "/123/abc";
+
+const links = [
+  "home",
+  "add driver",
+  "list drivers",
+  "delete driver",
+  "add package",
+  "list packages",
+  "delete package",
+];
+
+const drivers = [];
+const packages = [];
+
+module.exports = { url, links, drivers, packages };
