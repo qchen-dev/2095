4d6490e665ca73a13ec44d6eb2c0b222be577f5f
Q Chen <qche.au@gmail.com>
2024-08-30 00:11:20 +1000

    update router

--- routes/package.router.js
+++ routes/package.router.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { url, links } = require("../data");
+const {
+  url,
+  links,
+  drivers,
+  packages,
+  packageTableHeader,
+} = require("../data");
 
 router.get("/add-package", (req, res) => {
-  res.render("add-package", { url, links });
+  res.render("add-package", { url, links, drivers });
 });
 
 router.get("/list-packages", (req, res) => {
-  res.render("list-packages", { url, links });
+  res.render("list-packages", { url, links, packages, packageTableHeader });
 });
 
 router.get("/delete-package", (req, res) => {
