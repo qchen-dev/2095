7824eea2e876365adc3f544018f462c1ddd09401
Q Chen <qche.au@gmail.com>
2024-08-30 00:09:41 +1000

    add static style css

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.use(express.static("public"));
+app.use(express.static("public/css"));
 // app.use(express.static("node_modules/bootstrap/dist/css"));
 // app.use(
 //   "/css",
